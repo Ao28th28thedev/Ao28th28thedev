aec2d1275abb8c98871aa9fd3b369140ac2ff586
Ao28th28 <109283781+XPMUser@users.noreply.github.com>
2023-08-11 19:14:06 -0400

    Create game.js

---
+++ oldprodigy/1-70-0/js/game.js
@@ -0,0 +1,43 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+(function(){
+	var sw;
+	sw = window.sw || {};
+	sw.overlayManager = sw.overlayManager || new SW.OverlayManagerView({el: "#overlay-mask"});
+	sw.toastManager = new SW.ToastManager();
+	sw.dialogManager = sw.dialogManager || new SW.DialogManager();
+	sw.track = sw.track || new SW.Track();
+	sw.config = sw.config || new SW.Config();
+})();
+
+}
+/*
+     FILE ARCHIVED ON 14:26:29 Apr 02, 2020 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 22:42:05 Aug 11, 2023.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 456.122
+  exclusion.robots: 0.111
+  exclusion.robots.policy: 0.103
+  cdx.remote: 0.052
+  esindex: 0.009
+  LoadShardBlock: 428.485 (3)
+  PetaboxLoader3.resolve: 357.742 (4)
+  PetaboxLoader3.datanode: 197.873 (4)
+  load_resource: 165.475
+*/
